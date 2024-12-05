3b664d0126366deff6c0ce30c99e1cb1e2bba86f
Sofiane Terraf <73217929+sterra7518@users.noreply.github.com>
2024-12-05 18:30:15 +0100

    Add files via upload
    Création de la table des candidats politique ainsi que l'ajout des valeurs en SQL

---
+++ politique_sql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE 'politique' ('Index' INTEGER,'Political' TEXT,'Candidat' TEXT);
+
+
+INSERT INTO politique ('id','parti','candidat') VALUES 
+ ('1','Republicain','Trump'), 
+ ('2','Democrate','Clinton');
